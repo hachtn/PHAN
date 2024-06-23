9c60f89f5c113c5959e8ba2cc2a675d5ba716d7b
hachtn <phankd05@gmail.com>
2024-06-23 15:14:13 +0900

    最安値を達成する 1 Fix

--- Python/B_RANK/DPメニュー/最安値を達成するには 1.py
+++ Python/B_RANK/DPメニュー/最安値を達成するには 1.py
@@ -1,13 +1,11 @@
-n,a,b = map(int, input().split())
+n, a, b = map(int, input().split())
 
-dp = [0]*(n+1)
+dp = [10000000] * (n + 1)
 
-dp[1]=a
+dp[0] = 0
+dp[1] = a
 
-for i in range(2,n+1):
-    if dp[i-1] + a < dp[i-2] + b:
-        dp[i] = dp[i-1] + a 
-    else :
-        dp[i] = dp[i-2] + b
+for i in range(2, n + 1):
+    dp[i] = min(dp[i - 1] + a, dp[i - 2] + b)
 
 print(dp[n])
