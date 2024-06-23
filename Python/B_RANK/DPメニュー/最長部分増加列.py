1bf70ba5baba86705b167429ba0e15b2e79de7cd
hachtn <phankd05@gmail.com>
2024-06-23 21:40:29 +0900

    最長部分増加列

---
+++ Python/B_RANK/DPメニュー/最長部分増加列.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+a = [int(input()) for _ in range(n)]
+
+dp = [1] * n
+
+for i in range(1,n):
+    for j in range(i):
+        if a[j] < a[i]:
+            dp[i] = max(dp[i],dp[j]+1)
+
+print(max(dp))
+
+
