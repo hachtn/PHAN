461272f2ef5c89ef740bbb32a5a178c9c4698759
hachtn <phankd05@gmail.com>
2024-06-27 23:06:56 +0900

    最長部分増加列

--- Python/Test.py
+++ Python/Test.py
@@ -1,8 +1,54 @@
-import sys
+with open('D:\\Documents\\GitHub\\PHAN\\Python\\test.txt', 'r') as f:
+        data = f.read().strip().split()
 
-data = sys.stdin.read().strip().split()
+
+index = 0
+N = int(data[index])
+index += 1
+M = int(data[index])
+index += 1
+
+
+a = []
+for i in range(M):
+    a.append(int(data[index]))
+    index += 1
+
+
+h = []
+for i in range(M):
+    measures = []
+    for j in range(N):
+        measures.append(int(data[index]))
+        index += 1
+    h.append(measures)
+
+
+max_points = 0
+
+for j in range(N):  
+    pt = 100
+    for i in range(M):  
+        difference = abs(h[i][j] - a[i])
+        if difference <= 5:
+            continue 
+        elif difference <= 10:
+            pt -= 1
+        elif difference <= 20:
+            pt -= 2
+        elif difference <= 30:
+            pt -= 3
+        else:
+            pt -= 5
+        
+       
+        if pt < 0:
+            pt = 0
     
-N = int(data[0])
-a = data[1:]
-    
-    
+   
+    if pt > max_points:
+        max_points = pt
+
+
+print(max_points)
+
